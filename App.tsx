ec93b4571cad1085d960eca4828691f626f32b32
Marcely Pereira Costa <amarcelypereiracosta@gmail.com>
2026-03-04 13:03:33 -0300

    config(style-guide): Configuração do Style Guide de cores e fonte

--- App.tsx
+++ App.tsx
@@ -1,5 +1,19 @@
 import { Route } from "@/routes";
+import {
+  useFonts,
+  Lato_400Regular,
+  Lato_700Bold,
+} from "@expo-google-fonts/lato";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Lato_400Regular,
+    Lato_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Route />;
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,70 @@
+export const theme = {
+  colors: {
+    // Principal
+    purpleLight: "#DFDAF2",
+    purpleBase: "#6A46EB",
+
+    // Base
+    gray100: "#FAFAFA",
+    gray200: "#F0F0F0",
+    gray300: "#E6E5E5",
+    gray400: "#A1A2A1",
+    gray500: "#676767",
+    gray600: "#4A4A4A",
+    gray700: "#0F0F0F",
+
+    // Feedback
+    dangerLight: "#FFD6D6",
+    dangerBase: "#DB4D4D",
+    dangerDark: "#9E4949",
+
+    successLight: "#BFF7BE",
+    successBase: "#4BB84A",
+    successDark: "#30752F",
+
+    infoLight: "#CEEFFF",
+    infoBase: "#2AA1D9",
+    infoDark: "#1D7096",
+  },
+  fontFamily: {
+    regular: "Lato_400Regular",
+    bold: "Lato_700Bold",
+  },
+  typography: {
+    titleLg: {
+      fontSize: 18,
+      fontFamily: "Lato_700Bold",
+      lineHeight: 25.2, // 140%
+    },
+    titleMd: {
+      fontSize: 16,
+      fontFamily: "Lato_700Bold",
+      lineHeight: 22.4, // 140%
+    },
+    titleSm: {
+      fontSize: 14,
+      fontFamily: "Lato_700Bold",
+      lineHeight: 19.6, // 140%
+    },
+    titleXs: {
+      fontSize: 12,
+      fontFamily: "Lato_700Bold",
+      lineHeight: 16.8, // 140%
+    },
+    textMd: {
+      fontSize: 16,
+      fontFamily: "Lato_400Regular",
+      lineHeight: 22.4, // 140%
+    },
+    textSm: {
+      fontSize: 14,
+      fontFamily: "Lato_400Regular",
+      lineHeight: 19.6, // 140%
+    },
+    textXs: {
+      fontSize: 12,
+      fontFamily: "Lato_400Regular",
+      lineHeight: 16.8, // 140%
+    },
+  },
+};
